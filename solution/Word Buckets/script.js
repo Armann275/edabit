6755390f97522396c1388833aad1bfef71bbd288
Arman <xachatryanarman067@gmail.com>
2024-11-06 17:55:27 +0400

    Add Word Buckets file

---
+++ solution/Word Buckets/script.js
@@ -0,0 +1,29 @@
+function bucketize(phrase, n) {
+    const resultArr = [];
+	const splitedArr = phrase.split(" ");
+    let str = '';
+    
+    for(let i = 0; i < splitedArr.length; i++){
+        if (splitedArr[i].length > n) {
+            return []
+        }
+        if (!str) {
+            str += splitedArr[i];
+        }
+        if (splitedArr[i + 1]) {
+            if (str.length + splitedArr[i + 1].length + 1 > n) {
+            resultArr.push(str);
+            str = '';
+         }else{
+            str += ` ${splitedArr[i + 1]}`
+        }
+        }else{
+            resultArr.push(str)
+        }
+    }   
+    return resultArr
+}
+
+const res = bucketize("the mouse jumped over the cheese", 7)
+console.log(res);
+
